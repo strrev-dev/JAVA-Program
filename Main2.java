6a7e7b83e243d1369c375442e7ab65efc9a0d9df
strrev-dev <deepanmaurya699@gmail.com>
2025-04-05 03:48:17 +0530

    second commit

---
+++ Main2.java
@@ -0,0 +1,24 @@
+//How to print different type of values in Java?
+public class Main2{
+
+    public static void main(String[] args) {
+
+        System.out.println("Printing different type of values :");
+
+        int num = 234;
+        float  b;
+         char  c;
+         String s;
+
+         b = 34.83f;
+          c = 'A';
+          s ="hello world";
+
+          System.out.println("value of integer =" + num );
+          System.out.println("value of float =" + b);
+          System.out.println("value of character = " + c);
+          System.out.println("value of String = " + s );
+
+    }
+}
+
