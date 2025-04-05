752bd6befe65a0bc1228cd36240686e90295d853
strrev-dev <deepanmaurya699@gmail.com>
2025-04-06 03:37:23 +0530

    fourth commit

---
+++ Numbers.java
@@ -0,0 +1,18 @@
+//find sum  and average of two numbers
+import java.util.*;
+public class Numbers {
+    public static void main(String[] args) {
+        int num1, num2, sum;
+        double average;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number:");
+        num1 = sc.nextInt();
+        System.out.println(  "Enter second number:");
+        num2 = sc.nextInt();
+        sum = num1 + num2;
+        average = sum / 2.0;
+        System.out.println("Sum: " + sum);
+        System.out.println("Average: " + average);
+        sc.close();
+    }
+}
