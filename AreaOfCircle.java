748c4948ad525de097250fbafc02f3fe36fbf0e6
strrev-dev <deepanmaurya699@gmail.com>
2025-10-29 18:00:36 +0530

    area of circle

---
+++ AreaOfCircle.java
@@ -0,0 +1,12 @@
+
+import java.util.*;
+
+public class AreaOfCircle {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        float rad = scn.nextFloat();
+        float Area = 3.14f * rad * rad;
+        System.out.println(Area);
+    }
+}
