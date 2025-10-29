c131e8f27bdc89b95a74167fb4709dacbda0b7f0
strrev-dev <deepanmaurya699@gmail.com>
2025-10-29 12:10:09 +0530

    First commit of java

---
+++ JavaBasics.java
@@ -0,0 +1,33 @@
+
+import java.util.*;
+
+public class JavaBasics {
+
+    public static void main(String args[]) {
+
+        int a = 10;
+        int b = 5;
+
+        Scanner sc = new Scanner(System.in);
+        String inputName = sc.next();
+        System.out.println(inputName);
+        String name = "deepan";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(name);
+
+        System.out.println("Hello World"); //ln is used  for the next line or we can also use /n
+        System.out.println("Hello World");
+
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+
+        int sum = a + b;
+
+        System.out.println(sum);
+  
+        sc.close();
+    }
+}
