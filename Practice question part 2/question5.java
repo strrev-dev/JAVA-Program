28b8af7e229851ae0ed743bf072774bbe514c0cf
strrev-dev <deepanmaurya699@gmail.com>
2025-10-29 21:52:57 +0530

    practice questions part 2

---
+++ Practice question part 2/question5.java
@@ -0,0 +1,9 @@
+/* Question 5: (Advanced) Will the following statement give any error in Java?
+ int $ = 24 */
+
+public class question5 {
+    public static void main(String[] args) {
+        int $ = 24;
+        System.out.println($);
+    }
+}
