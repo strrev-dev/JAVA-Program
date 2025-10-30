81577cb72255fbc8e88c41aa240a33d42e9a8b3c
strrev-dev <deepanmaurya699@gmail.com>
2025-10-30 22:07:02 +0530

    Arithmetic operator

---
+++ ArithmeticOperator.java
@@ -0,0 +1,12 @@
+public class ArithmeticOperator {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 12;
+
+        System.out.println("Add = " + (a+b));
+        System.out.println("Sub = " + (a-b));
+        System.out.println("Multiplication = " + (a*b));
+        System.out.println("Divide = " + (a/b));
+        System.out.println("Modulus = " + (a%b));
+    }
+}
