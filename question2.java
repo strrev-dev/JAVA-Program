96fe77a28ab4596db1a43eb060586b17c0afd1ab
strrev-dev <deepanmaurya699@gmail.com>
2025-11-05 23:02:21 +0530

    checking fever or not

---
+++ question2.java
@@ -0,0 +1,15 @@
+/* Question2: FinishthefollowingcodesothatitprintsYouhaveafeverifyourtemperature
+ is above 100 and otherwise prints You don't have a fever. */
+public class question2 {
+    public static void main(String[] args) {
+        double temp = 103.5;
+        
+        if(temp >=100){
+            System.out.println("fever");
+        }
+        else {
+            System.out.println("NOt a fever");
+        }
+
+    }
+}
