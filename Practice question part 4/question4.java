bd44cdb1449be597bacf282c7f159bb6a118e3c3
strrev-dev <deepanmaurya699@gmail.com>
2025-11-05 23:14:14 +0530

    using ternary operator find the value of x and y

---
+++ Practice question part 4/question4.java
@@ -0,0 +1,12 @@
+
+public class question4 {
+
+    public static void main(String[] args) {
+        int a = 63, b = 36;
+        boolean x = (a < b) ? true : false;
+        int y = (a > b) ? a : b;
+        System.out.println("The value of x : " + x);
+        System.out.println("The value of x : " + y);
+    }
+}
+
