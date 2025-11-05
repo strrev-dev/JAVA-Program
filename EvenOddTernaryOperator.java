1db27e59c5e79e09cfae861c7ac7281500ca006f
strrev-dev <deepanmaurya699@gmail.com>
2025-11-05 21:27:53 +0530

    Ternanary operator even odd string

---
+++ EvenOddTernaryOperator.java
@@ -0,0 +1,8 @@
+public class EvenOddTernaryOperator {
+    public static void main(String[] args) {
+        int number = 9;
+
+        String type = number%2==0?"even":"odd";
+        System.out.println(type);
+    }
+}
