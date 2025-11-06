3b0d7e32bc18cb7dccba94ea012d5b037460d92a
strrev-dev <deepanmaurya699@gmail.com>
2025-11-07 00:05:29 +0530

    Print the number from 1 to 100

---
+++ Print1To100.java
@@ -0,0 +1,10 @@
+public class Print1To100 {
+    public static void main(String[] args) {
+        int Count = 1;
+
+        while(Count<=100){
+            System.out.println(Count);
+            Count++;
+        }
+    }
+}
